99d5a217814694540a1bb6357ae5da872eb48e87
Hassan Abass <abass.hassan.7798@gmail.com>
2022-03-21 00:30:17 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,139 @@
+first = () => {
+  let num = 0;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "you guessed right, the Number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+
+second = () => {
+  let num = 1;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+
+third = () => {
+  let num = 2;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+
+fourth = () => {
+  let num = 3;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+fifth = () => {
+  let num = 4;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+
+sixth = () => {
+  let num = 5;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+
+seventh = () => {
+  let num = 6;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+eight = () => {
+  let num = 7;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+
+nineth = () => {
+  let num = 8;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+tenth = () => {
+  let num = 9;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 11);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
+
+eleventh = () => {
+  let num = 10;
+  let pick = document.getElementById("result");
+  const guess = Math.round(Math.random() * 10);
+  if (guess == num) {
+    pick.innerHTML = "You guessed right, the number is" + " " + guess;
+    pick.style.backgroundColor = "green";
+  } else {
+    pick.innerHTML = "Wrong guess, try again!!!";
+    pick.style.backgroundColor = "red";
+  }
+};
